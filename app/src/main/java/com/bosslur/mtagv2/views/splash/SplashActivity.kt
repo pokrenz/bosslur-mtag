ccdce010b9b1bf12028dc4ead708d61ce44bf97e
Miftah Agustian <pokrenz@gmail.com>
2021-01-26 23:02:39 +0700

    add ui-splash view activity

---
+++ app/src/main/java/com/bosslur/mtagv2/views/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.bosslur.mtagv2.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bosslur.mtagv2.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
