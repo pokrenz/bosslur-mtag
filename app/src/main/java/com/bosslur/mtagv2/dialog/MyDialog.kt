a2f2f8da190fa11a671d64e31e66eb1e6f474eaa
Miftah Agustian <pokrenz@gmail.com>
2021-01-31 01:59:35 +0700

    ui: custom dialog and progress bar

---
+++ app/src/main/java/com/bosslur/mtagv2/dialog/MyDialog.kt
@@ -0,0 +1,34 @@
+package com.bosslur.mtagv2.dialog
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import com.bosslur.mtagv2.R
+
+object MyDialog {
+    private var dialogBuilder: AlertDialog? = null
+
+    fun dynamicDialog(context: Context?,title: String?,message: String?){
+        dialogBuilder = AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .show()
+    }
+
+    fun showProgressDialog(context: Context?){
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.layout_progress,null)
+        dialogBuilder = AlertDialog.Builder(context)
+            .setView(dialogView)
+            .setCancelable(false)
+            .create()
+
+        dialogBuilder?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialogBuilder?.show()
+    }
+
+    fun hideDialog(){
+        if (dialogBuilder != null){
+            dialogBuilder?.dismiss()
+        }
+    }
+}
