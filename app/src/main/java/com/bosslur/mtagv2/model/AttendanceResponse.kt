2ef90141f4417c6f28d5be696a579480b446cbc7
Miftah Agustian <pokrenz@gmail.com>
2021-01-31 18:05:43 +0700

    Feat: create attendance to api

---
+++ app/src/main/java/com/bosslur/mtagv2/model/AttendanceResponse.kt
@@ -0,0 +1,9 @@
+package com.bosslur.mtagv2.model
+
+import com.google.gson.annotations.SerializedName
+
+data class AttendanceResponse(
+
+        @field:SerializedName("message")
+        val message: String? = null
+)
